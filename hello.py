49249b974c16ffa7c2ca889ee4f2ef88f30fe6ee
Jada Sujan Kumar <62364934+sujan381@users.noreply.github.com>
2025-09-04 09:05:45 +0530

    worked on git from vs code editor

--- hello.py
+++ hello.py
@@ -3,3 +3,5 @@ print("Hello Sujan")
 print("Hello Naveen")
 
 print("hello from github desktop")
+
+print("Hello from vs code")
--- hello1.py
+++ hello1.py
@@ -2,3 +2,5 @@ print("Hello")
 print("Hello from github desktop")
 
 print("working on git statsh")
+
+print("worked on git hub from vs code")
